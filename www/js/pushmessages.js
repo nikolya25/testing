0bcfa45dad37b7afd137f7860aff571fcd145fd4
Nikolya <kolya_shchur@mail.ru>
2017-05-12 18:47:22 +0200

    app42 test

--- www/js/pushmessages.js
+++ www/js/pushmessages.js
@@ -1,29 +1,59 @@
-PushNotification.init({
-    android: {
-        senderID: "806082497848"
-    }
-});
+var userName = "Nick",  
+	deviceToken = "Device Token",
+	channel = "News Channel",  
+	description = "Channel all about the news", 
+	message  = "Message which you have to send",  
+	result;    
 
-PushNotification.hasPermission(function(data) {
-    if (data.isEnabled) {
-        console.log('isEnabled');
-    }
-});
+App42.initialize("cbd62cb6d46c4cffc5730955a9f9f1319a79afbcddbc12daf71ef808d40fbbdd","a1ce760466ea6cd6e54238a0938b4a9b712ba40379e147957e3707039a0d90b7");
 
-push.on('registration', function(data) {
-	console.log(data.registrationId);
-});
+var pushNotificationService  = new App42Push();  
 
-push.on('notification', function(data) {
-	console.log(data.message);
-	console.log(data.title);
-	console.log(data.count);
-	console.log(data.sound);
-	console.log(data.image);
-	console.log(data.additionalData);
+pushNotificationService.storeDeviceToken(userName, deviceToken,DeviceType.ANDROID,{    
+    success: function(object)   
+    {    
+        var pushNotification = JSON.parse(object);    
+        result = pushNotification.app42.response.push;  
+        console.log("UserName is : " + result.userName)   
+        console.log("Type is : " +  result.type);  
+        console.log("DeviceToken is : " +  result.deviceToken);   
+    },    
+    error: function(error) {    
+    }    
+});  
 
-});
+pushNotificationService.createChannelForApp(channel, description,{    
+    success: function(object)   
+    {    
+        var pushNotification = JSON.parse(object);    
+        result = pushNotification.app42.response.push.channels.channel;  
+        console.log("ChannelName is : " + result.channelName)     
+        console.log("Description is: " +  result.description);    
+    },    
+    error: function(error) {    
+    }    
+}); 
 
-push.on('error', function(e) {
-	console.log(e.message);
-});
+pushNotificationService.subscribeToChannel(channel, userName,{    
+    success: function(object)   
+    {    
+        var pushNotification = JSON.parse(object);    
+        result = pushNotification.app42.response.push.channels.channel;  
+        console.log("UserName is : " + pushNotification.app42.response.push.userName)     
+        console.log("ChannelName is : " + result.channelName)     
+    },    
+    error: function(error) {    
+    }    
+});  
+
+pushNotificationService.sendPushMessageToAll(message, {    
+    success: function(object)   
+    {    
+        var pushNotification = JSON.parse(object);    
+        result = pushNotification.app42.response.push;  
+        console.log("Message is : " + result.message)     
+        console.log("Expiry is : " + result.expiry)   
+    },    
+    error: function(error) {    
+    }    
+}); 
