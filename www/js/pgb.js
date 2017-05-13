854f09a17e99c52134e1baab0f9caaafcdd3667d
Nikolya <kolya_shchur@mail.ru>
2017-05-13 20:16:33 +0200

    closest station 14

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -107,7 +107,7 @@ function myPositionAndClosestStation() {
 		document.getElementById('test1').innerHTML = data[2].position.lng;
 		for (var i = 0; i < data.length; i++) {
 			stla[i] = Math.sqrt(Math.pow(myLat - data[i].position.lat, 2) + Math.pow(myLng - data[i].position.lng, 2));
-			document.getElementById('test2').innerHTML = stla[i].position.lat;		
+			document.getElementById('test2').innerHTML = stla[i];		
 		}
 	}
 
