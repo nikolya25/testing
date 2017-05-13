d2c7b074a75047689ae01f38abe778adfe716944
Nikolya <kolya_shchur@mail.ru>
2017-05-13 15:39:33 +0200

    data on map test10

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -79,7 +79,11 @@ function stationsOnMap() {
 		for (var i = 0; i < data.length; i++) {
 			map.addMarker(data[i], function(marker) {
 				marker.addEventListener(plugin.google.maps.event.INFO_CLICK, function() {
-					alert("Stan powietrza: " + stan1.stIndexLevel.indexLevelName);
+					document.getElementById('test1').innerHTML = document.getElementById('station1Stan').value;
+					document.getElementById('test1').innerHTML = document.getElementById('station1Stan');
+					document.getElementById('test1').innerHTML = $('#station1Stan').value;
+					document.getElementById('test1').innerHTML = $('#station1Stan');
+					alert("Stan powietrza: " + document.getElementById('station1Stan').value);
 				});
 			});
 		}
