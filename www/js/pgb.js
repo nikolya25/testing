54a53552296d66b2ec1f9d13ae7ce873e79803a5
Nikolya <kolya_shchur@mail.ru>
2017-05-13 10:03:40 +0200

    button out

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -32,11 +32,6 @@ function onDeviceReady() {
 }
 
 function onMapReady() {
-	var stButton = document.getElementById("stationsOnMap");
-	stButton.addEventListener("click", onBtnClicked);
-}
-
-function onBtnClicked() {
 	var data = [
 		{
 			'position': {lat: 50.057678, lng: 19.926189},
