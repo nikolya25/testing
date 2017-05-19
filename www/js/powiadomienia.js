020d5354548a1323415e4453c2329a1589fefb25
Nikolya <kolya_shchur@mail.ru>
2017-05-19 20:26:34 +0200

    fixes 11

--- www/js/powiadomienia.js
+++ www/js/powiadomienia.js
@@ -6,13 +6,13 @@
 
 function checkRadio() {
 	if(document.getElementById('1godzina').checked) {
-  		var interwal = "hour";
+  		var interwal = 60*60*1000;
   		return interwal;
 	}else if(document.getElementById('24godziny').checked) {
-	  	var interwal = "day";
+	  	var interwal = 24*60*60*1000;
 	  	return interwal;
 	}else if(document.getElementById('1minuta').checked) {
-		var interwal = 10*1000;
+		var interwal = 60*1000;
 		return interwal;	
 	}
 }
@@ -28,8 +28,14 @@ function checkStation(){
 	return nazwaWybranejStacji;
 }
 
+function checkState(){
+	var wybranyStan = document.getElementById('stateChoice').options[document.getElementById('stateChoice').selectedIndex].value;
+	localStorage.setItem("wybranyStan", wybranyStan);
+	return wybranyStan;
+}
 
 function ustawPowiadomienie(){
+	//clearInterval(timer);
 	var interwal = checkRadio();
 	var pozwolenie = checkSlider();
 	var stacja = checkStation();
@@ -49,20 +55,52 @@ function ustawPowiadomienie(){
 function wykonajPomiar(){
 	$.getJSON( "http://api.gios.gov.pl/pjp-api/rest/aqindex/getIndex/"+localStorage.getItem("stacja"), function( stan3 ) {
 		pomiarPowiadomienie = stan3.stIndexLevel.indexLevelName;
-		powiadomienia(pomiarPowiadomienie);
+		switch (checkState()) {
+		case "Bardzo dobry":
+			powiadomienia(pomiarPowiadomienie);
+			console.log(pomiarPowiadomienie);
+			break;
+		case "Dobry":
+			if (pomiarPowiadomienie === "Dobry" || pomiarPowiadomienie === "Umiarkowany" || pomiarPowiadomienie === "Dostateczny" || pomiarPowiadomienie === "Zły" || pomiarPowiadomienie === "Bardzo zły") {
+				powiadomienia(pomiarPowiadomienie);
+				console.log(pomiarPowiadomienie);
+			}
+			break;
+		case "Umiarkowany":
+			if (pomiarPowiadomienie === "Umiarkowany" || pomiarPowiadomienie === "Dostateczny" || pomiarPowiadomienie === "Zły" || pomiarPowiadomienie === "Bardzo zły") {
+				powiadomienia(pomiarPowiadomienie);
+				console.log(pomiarPowiadomienie);
+			}
+			break;
+		case "Dostateczny":
+			if (pomiarPowiadomienie === "Dostateczny" || pomiarPowiadomienie === "Zły" || pomiarPowiadomienie === "Bardzo zły") {
+				powiadomienia(pomiarPowiadomienie);
+				console.log(pomiarPowiadomienie);
+			}
+			break;
+			case "Zły":
+			if (pomiarPowiadomienie === "Zły" || pomiarPowiadomienie === "Bardzo zły") {
+				powiadomienia(pomiarPowiadomienie);
+				console.log(pomiarPowiadomienie);
+			}
+			break;
+		case "Bardzo zły":
+			if (pomiarPowiadomienie === "Bardzo zły") {
+				powiadomienia(pomiarPowiadomienie);
+				console.log(pomiarPowiadomienie);
+			}
+		}
 	});
 }
-
-
-
 
 function powiadomienia(pomiarPowiadomienieData){
 	cordova.plugins.notification.local.schedule({
 	  id: 1,
 	  title: localStorage.getItem("nazwaStacji"),
 	  text: 'Stan powietrza: ' + pomiarPowiadomienieData,
+	  //sound: null,
 	  autoClear: false,
-	  at: new Date(new Date().getTime() + 5*1000)
+	  //at: new Date(new Date().getTime() + 5*1000)
 	});
 }
 
